ea49b18501a4460a9cb7133e65bb353d8ae72371
DESKTOP-2TIVCA1\inha <awe4656@gmail.com>
2026-07-30 11:57:59 +0900

    feat : texture class에 변수 추가

--- JumpKing/Texture.h
+++ JumpKing/Texture.h
@@ -10,7 +10,16 @@ public:
 	
 	int32 GetWidth() const;
 	int32 GetHeight() const;
+	SIZE GetFrameSize() { return SIZE(_frameWidth, _frameHeight); }
 
 private:
 	unique_ptr<Gdiplus::Image> _image;
+
+	uint32 _width = 0;
+	uint32 _height = 0;
+
+	int32 _row = 1;
+	int32 _col = 1;
+	int32 _frameWidth = 0;
+	int32 _frameHeight = 0;
 };
