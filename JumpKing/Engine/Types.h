3f40e0997d4ba3bf9f723d50c91fb55757bbda97
DESKTOP-2TIVCA1\inha <awe4656@gmail.com>
2026-07-30 09:37:01 +0900

    feat : enum class 액터 타입, 레이어 정의

--- JumpKing/Engine/Types.h
+++ JumpKing/Engine/Types.h
@@ -18,6 +18,27 @@ enum class SceneType
     Count
 };
 
+enum class ActorType
+{
+    Background,
+    Foreground,
+    Mainground,
+    Effect,
+    Player,
+};
+
+enum class RenderLayer
+{
+    Background,
+    Mainground,
+    Player,
+    Foreground,
+    Effect,
+
+    // 최대 개수
+    Count
+};
+
 constexpr int32 GAME_SCREEN_WIDTH = 800;
 constexpr int32 GAME_SCREEN_HEIGHT = 720;
 constexpr float TARGET_FRAME_RATE = 120.0f;
