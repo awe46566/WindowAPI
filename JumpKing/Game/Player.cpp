bcc26afc4af17051adbdd694ef708283ac426a89
DESKTOP-2TIVCA1\inha <awe4656@gmail.com>
2026-08-11 11:48:50 +0900

    feat : Slope 콜라이더 충돌처리 구현

--- JumpKing/Game/Player.cpp
+++ JumpKing/Game/Player.cpp
@@ -288,21 +288,47 @@ void Player::VerticalCollision(Vector2& nextPosition)
 
 float Player::GetSlopeSurfaceY(const PlatformData& platform, float x) const
 {
-	// TODO(user): x를 [bounds.Left(), bounds.Right()]로 clamp한 뒤,
+	// TODO : x를 [bounds.Left(), bounds.Right()]로 clamp한 뒤,
 	// LevelData.h의 IsSlopeRisingRight/IsSlopeFloor 규칙대로
 	// 대각선 위의 y를 선형보간해서 반환한다.
-	return platform.bounds.Top();
+	x = max(platform.bounds.Left(), min(x, platform.bounds.Right()));
+
+	Vector2 left, right;
+
+	GetSlopeEndpoints(platform, left, right);
+	float t = (x - left.x) / (right.x - left.x);
+	float y = left.y + t * (right.y - left.y);
+
+	return y;
 }
 
 bool Player::ResolveSlopeCollision(const PlatformData& platform, Vector2& nextPosition)
 {
-	// TODO(user): 계획 문서(unified-jingling-brooks.md) 2번 항목 참고.
+	// TODO 
 	// 1) _collider->GetBounds(nextPosition)와 platform.bounds가 수평으로 겹치는지 확인
 	// 2) 콜라이더 중심 x에서 GetSlopeSurfaceY로 표면 y 계산
 	// 3) floor면 콜라이더 bottom을, ceiling이면 top을 표면에 스냅
 	// 4) _velocity.y = 0, AirBorne 상태였다면 OnLanded() 호출
 	// 5) floor에 착지했다면 _isGrounded=true, _groundMaterial=platform.material,
 	//    _groundSlope=platform.slope 로 기록 (미끄러짐 로직이 여기서 참조함)
+	const Rect nextBounds = _collider->GetBounds(nextPosition);
+	
+	if (nextBounds.Left() < platform.bounds.Right() 
+		&& nextBounds.Right() > platform.bounds.Left())
+	{
+		float centerX = (nextBounds.Right() + nextBounds.Left()) * 0.5f;
+		float surfaceY = GetSlopeSurfaceY(platform, centerX);
+
+		if (IsSlopeFloor(platform))
+		{
+			nextPosition.y += surfaceY - nextBounds.Bottom();
+		}
+		else
+		{
+			nextPosition.y += surfaceY - nextBounds.Top();
+		}
+	}
+
 	return false;
 }
 
