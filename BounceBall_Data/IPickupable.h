6831fd3d897aa1a33456d55b11f7144a01d0c0a2
awe46566 <awe4656@gmail.com>
2026-08-19 21:44:52 +0900

    chore : 불필요 파일 제거

--- BounceBall_Data/IPickupable.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class IPickupable
-{
-public:
-    virtual ~IPickupable() = default;
-
-    virtual void OnPickedUp(class Ball* ball) = 0;
-};
--- BounceBall_Data/UIImage.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Widget.h"
-
-class UIImage : public Widget
-{
-};
-
